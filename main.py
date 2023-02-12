af676c8f4c2b1614fc5edb099f5824d7e7727f28
Beck Bjella <beckbjella@gmail.com>
2023-02-12 02:06:43 -0600

    mostly finished vision code

--- main.py
+++ main.py
@@ -1,33 +1,50 @@
 # from cscore import CameraServer
 # from networktables import NetworkTables
-import numpy
+import numpy as np
 import cv2
 from grip import GripPipeline
+import math
 
 
 class ContourData:
     con = None
     area = None
-    center = None
-    perimeter = None
 
-    def __init__(self, con, center, area, perimeter) -> None:
+    def __init__(self, con, area) -> None:
         self.con = con
-        self.center = center
         self.area = area
-        self.perimeter = perimeter
+
+
+class LineData:
+    p1 = None
+    p2 = None
+    length = None
+    center = None
+
+    def __init__(self, p1, p2) -> None:
+        self.p1 = p1
+        self.p2 = p2
+        self.length = math.dist(p1, p2)
+        self.center = (int((p1[0] + p2[0])/2), int((p1[1]+p2[1])/2))
+
+
+def get_line_length(line):
+    return line.length
 
 
 def main():
-    left_threshold = 0.20
-    right_threshold = 0.80
+    # left_threshold = 0.20
+    # right_threshold = 0.80
 
-    screen_width = 480
-    screen_height = 360
+    screen_width = 640
+    screen_height = 480
 
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
     cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0)
-    cap.set(cv2.CAP_PROP_EXPOSURE, -7)
+    cap.set(cv2.CAP_PROP_EXPOSURE, -4)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, screen_height)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, screen_width)
 
     # ----------------------------
 
@@ -46,25 +63,15 @@ def main():
     pipeline = GripPipeline()
 
     while True:
-        ret, frame = cap.read()
-
-        cv2.imshow('original', frame)
+        _, frame = cap.read()
 
         pipeline.process(frame)
         contours = pipeline.find_contours_output
 
         contour_data = []
-
         for i, con in enumerate(contours):
-            m = cv2.moments(con)
-            cx = int(m["m10"] / m["m00"])
-            cy = int(m["m01"] / m["m00"])
-            center = (cx, cy)
-
             area = cv2.contourArea(con)
-            perimeter = cv2.arcLength(con, True)
-
-            contour_data.append(ContourData(con, center, area, perimeter))
+            contour_data.append(ContourData(con, area))
 
         best_contour = None
         for data in contour_data:
@@ -76,19 +83,52 @@ def main():
 
         if not best_contour == None:
             con = best_contour.con
-            center = best_contour.center
             area = best_contour.area
-            perimeter = best_contour.perimeter
 
-            cv2.drawContours(frame, [con], 0, (0, 255, 0), 3)
-            cv2.circle(frame, center, 5, (0, 255, 0), 8)
+            hull = cv2.convexHull(con)
+            hull_points = []
+            for p in hull:
+                hull_points.append((p[0][0], p[0][1]))
 
-        # cv2.putText(frame, str(area), center,
-        #             cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
-        # cv2.putText(frame, str(perimeter), (center[0], center[1] + 50),
-        #             cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+            lines = []
+            for i in range(len(hull_points)):
+                first_point = hull_points[i]
 
-        cv2.imshow('output', frame)
+                if i == len(hull_points) - 1:
+                    second_point = hull_points[0]
+                else:
+                    second_point = hull_points[i + 1]
+
+                lines.append(LineData(first_point, second_point))
+
+            lines.sort(reverse=True, key=get_line_length)
+            sides = lines[0:2]
+
+            cross_sections = [LineData(sides[0].p1, sides[1].p2), LineData(sides[0].p2, sides[1].p1)]
+            cross_sections.sort(reverse=True, key=get_line_length)
+            
+            bottom_line = cross_sections[0]
+            top_line = cross_sections[1]
+            
+            
+
+            cv2.line(frame, bottom_line.p1, bottom_line.p2, (0, 0, 0), 4)
+            cv2.line(frame, top_line.p1, top_line.p2, (0, 0, 0), 4)
+
+            cv2.line(frame, bottom_line.p1, top_line.p1, (0, 0, 0), 4)
+            cv2.line(frame, bottom_line.center, top_line.center, (0, 0, 0), 4)
+            cv2.line(frame, bottom_line.p2, top_line.p2, (0, 0, 0), 4)
+
+            cv2.circle(frame, top_line.p1, 5, (255, 255, 255), 8)
+            cv2.circle(frame, top_line.center, 5, (255, 255, 255), 8)
+            cv2.circle(frame, top_line.p2, 5, (255, 255, 255), 8)
+
+            cv2.circle(frame, bottom_line.p1, 5, (255, 255, 255), 8)
+            cv2.circle(frame, bottom_line.center, 5, (255, 255, 255), 8)
+            cv2.circle(frame, bottom_line.p2, 5, (255, 255, 255), 8)
+
+        cv2.imshow('final', frame)
+        cv2.imshow('dilate', pipeline.cv_dilate_output)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
